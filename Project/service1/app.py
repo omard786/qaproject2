3f5e832f68af94feda8f957c7bc387832c78cb88
omard786 <omardhin@gmail.com>
2021-03-29 14:42:52 +0000

    got my number generator working

---
+++ Project/service1/app.py
@@ -0,0 +1,12 @@
+import random 
+#this code needs to run 8 times for my list 
+number1 = random.randint(1, 8)
+number2 = random.randint(1, 8)
+number3 = random.randint(1, 8)
+number4 = random.randint(1, 8)
+number5 = random.randint(1, 8)
+number6 = random.randint(1, 8)
+number7 = random.randint(1, 8)
+number8 = random.randint(1, 8)
+random_num_list=(f"{number1},{number2},{number3},{number4},{number5},{number6},{number7},{number8}")
+print(random_num_list)
