736b6b3ebe16e04327dcdfcfb30131a831b40544
omard786 <omardhin@gmail.com>
2021-04-02 12:07:46 +0000

    got my number code working

--- Project/S3_number/app.py
+++ Project/S3_number/app.py
@@ -1,26 +1,20 @@
 import random 
-#this code needs to run 8 times for my list 
 
-odd_numbers= (1,3,5,7)
-even_numbers= (2,4,6,8)
+number_list = ['1','2','3','4','5','6','7','8']
+even = '2468'
+odd = '1357'
+#choices let you pick the letters from the lise, k=amount of objects from the list, weights is the prob of occuring  
+number= random.choices(number_list, weights=[1, 1, 1, 1, 1, 1, 1, 1], k=4)
 
-number1 = random.randint(1, 8)
-number2 = random.randint(1, 8)
-number3 = random.randint(1, 8)
-number4 = random.randint(1, 8)
-number5 = random.randint(1, 8)
-number6 = random.randint(1, 8)
-number7 = random.randint(1, 8)
-number8 = random.randint(1, 8)
-random_num_list=(f"{number1}{number2}{number3}{number4}{number5}{number6}{number7}{number8}")
-# print(random_num_list)
-print("-".join(random_num_list))
+print('these are your numbers :')
+dash_list= print('-'.join(number))
 
-even_number_occurances=random_num_list.count("2")
-print("this list has the number 2, this many times:",even_number_occurances)
-
-# for num in range(random_num_list):
-#     if num % 2 == 0:
-#         print("this is even numbers")
+#sort it so the letters dont reproduce 
 
 
+#list comphrension, using one list with another 
+even_count = [i for i in number if i in even]
+print ('you have this many even numbers : ', len(even_count))
+
+odd_count = [i for i in number if i in odd]
+print ('you have this many odd numbers : ', len(odd_count))
