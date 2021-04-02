24fb1a5ff3bd4bfd8076bb4aff5fb0f8d5ddacbb
omard786 <omardhin@gmail.com>
2021-04-02 09:50:14 +0000

    setting up nexus repo

--- Project/S3_number/jot.py
+++
@@ -1,8 +0,0 @@
-name = "Pep Guardogiola"
-age = 3
-bark = True
-tweet = False
-
-print("My pet is called", name, ", He is", age, "years old.")
-print("Statement:", name, "barks.", bark)
-print("Statement:", name, "tweets.", tweet)
