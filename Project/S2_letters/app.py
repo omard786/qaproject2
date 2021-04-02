32e3c7b50a6d163895d7d44d9aa8840211aebb9d
omard786 <omardhin@gmail.com>
2021-04-02 11:43:35 +0000

    got a letter gen, which shows how many vowels

--- Project/S2_letters/app.py
+++ Project/S2_letters/app.py
@@ -1,8 +1,17 @@
 import random 
 
-letter_generator = ['A','E','I','U','B','F','J','V']
+letter_list = ['A','E','I','U','B','F','J','V']
+vowels = 'AEIU'
+consonant = 'BFJV'
+#choices let you pick the letters from the lise, k=amount of objects from the list 
+letter= random.choices(letter_list, k=4)
 
-letter= random.choices(letter_generator, k=4)
-gen=print(letter)
+print('these are your letters :')
+dash_list= print('-'.join(letter))
 
-print('this is your letters',letter)
+#sort it so the letters dont reproduce 
+
+
+#list comphrension, using one list with another 
+vowel_count = [i for i in letter if i in vowels]
+print ('you have this many vowels : ', len(vowel_count))
